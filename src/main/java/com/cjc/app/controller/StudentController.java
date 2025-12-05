e5708098fd86f76e6d30391233e4d3d1526a1ccf
saurabh-tipule <tipulesaurabh12@gmail.com>
2025-12-05 16:57:35 +0530

    String variable added

--- src/main/java/com/cjc/app/controller/StudentController.java
+++ src/main/java/com/cjc/app/controller/StudentController.java
@@ -10,7 +10,9 @@ import com.cjc.app.model.Student;
 @Controller
 public class StudentController {
 
-         int a = 10;
+	String s = "ABC";
+
+	int a = 10;
 
 	public boolean findEvenoddNumber(int num) {
 		return (num % 2 == 0) ? true : false;
@@ -31,11 +33,9 @@ public class StudentController {
 		return false;
 	}
 
-        public String findNumber(int num)
-        {
-          return (num>0)?"Positive Number":"Negative Number";
-        }
-
+	public String findNumber(int num) {
+		return (num > 0) ? "Positive Number" : "Negative Number";
+	}
 
 	@RequestMapping(value = "/")
 	public String landingPage() {
